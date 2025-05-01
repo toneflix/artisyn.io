ea6ddd8e1d23f50d4fb727b4ca03e2e0193deaea
Favour <gismozfexie@yahoo.com>
2025-05-01 12:45:05 +0100

    chore: add browse category section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/landing/sections/hero';
 import HowItWorks from '@/components/landing/sections/how-it-works';
+import BrowseByCategory from '@/components/landing/sections/browse-by-category';
 
 function Page() {
   return (
     <div>
       <Hero />
       <HowItWorks />
+      <BrowseByCategory />
     </div>
   )
 };
